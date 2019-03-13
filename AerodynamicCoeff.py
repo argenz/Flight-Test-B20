c042e4eda722b0ba16e18bc4e39e5825eff9c805
Francesca <47746078+argenz@users.noreply.github.com>
2019-03-13 11:04:12 +0100

    Add files via upload

--- AerodynamicCoeff.py
+++ AerodynamicCoeff.py
@@ -10,7 +10,8 @@ from ISAmodule import *
 from math import *
 from Cit_par import *
 import matplotlib.pyplot as plt
-
+import numpy as np
+from scipy import stats
 mppl = get_Data1()[0]                    #mass of people
 oew = 3504                               #kg 
 mfuel = get_Data1()[1]                   #mass of fuel 
@@ -36,14 +37,54 @@ for i in range(len(IAS)):                #convert to True airspeed
 
 alpha = get_Data1()[5]
 
-plt.figure()
-plt.plot(alpha, CL)
-plt.show()
-   
-
-#TAT = get_Data1()[9]                    #temp
-
+#plt.figure()
+#plt.plot(alpha, CL)
+#plt.show()
+#   
+Tl = [3642.21, 2996.15, 2413.35, 1882.48, 1918.05, 2235.97]	
+Tr = [3745.21, 3057.76, 2540.09, 2035.8, 	2100.45, 2433.76]
+	
 T = []
+CD = []
+for i in range(len(Tl)):
+    t = (Tl[i]+Tr[i])/2
+    T.append(t)	
+    cd = t/(0.5*rho[i]*VAS[i]**2*S)
+    CD.append(cd)
+    
+#FINDING CD0 
+poli = np.polyfit(CL, CD, 2)
+CL1 = np.linspace(0, CL[-1], 20)
+CD1 =  []
+for i in range(len(CL1)):
+    cd = poli[0]*CL1[i]**2 + poli[1]*CL1[i] + poli[2]
+    CD1.append(cd)
+    
+CD0 = CD1[0]
+
+#FINDING OSWALD EFFICIENCY
+
+E =[]
+for i in range(len(CD)):
+    e = CL[i]**2/((CD[i]-CD0)*A*pi)
+    E.append(e)
+
+slope = stats.linregress(np.array(CL)**2, CD)[0]
+e = 1/(slope*A*pi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
