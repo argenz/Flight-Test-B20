07c1914a2c4892e770652ba9bf1ca8d2ea06e146
Francesca <47746078+argenz@users.noreply.github.com>
2019-03-20 09:41:55 +0100

    Add files via upload
    correct data reading

--- readmat.py
+++ readmat.py
@@ -41,85 +41,90 @@ Oil_pressure1 = [fd[7][i][0] for i in range(len(fd[7]))] #Engine 1: Oil pressure
 
 Oil_pressure2 = [fd[8][i][0] for i in range(len(fd[8]))] #Engine 2: Oil pressure
 
-Fan_speed1 = [fd[9][i][0] for i in range(len(fd[9]))] #Engine 1: Fan speed (N1)
+Deflection_of_Control_Column = [fd[9][i][0] for i in range(len(fd[10]))] #Stick Deflection
 
-Turbine_speed1 = [fd[10][i][0] for i in range(len(fd[10]))] #Engine 1: Turbine speed (N2)
+Fan_speed1 = [fd[10][i][0] for i in range(len(fd[9]))] #Engine 1: Fan speed (N1)
 
-Fan_speed2 = [fd[11][i][0] for i in range(len(fd[11]))] #Engine 2: Fan speed (N1)
+Turbine_speed1 = [fd[11][i][0] for i in range(len(fd[10]))] #Engine 1: Turbine speed (N2)
 
-Turbine_speed2 = [fd[12][i][0] for i in range(len(fd[12]))] #Engine 2: Turbine speed (N2)
+Fan_speed2 = [fd[12][i][0] for i in range(len(fd[11]))] #Engine 2: Fan speed (N1)
 
-Fuel1 = [fd[13][i][0] for i in range(len(fd[13]))]  #Engine 1
+Turbine_speed2 = [fd[13][i][0] for i in range(len(fd[12]))] #Engine 2: Turbine speed (N2)
 
-Fuel2 = [fd[14][i][0] for i in range(len(fd[14]))]  #Engine 2
+Fuel1 = [fd[14][i][0] for i in range(len(fd[13]))]  #Engine 1
 
-Deflection_of_aileron = [fd[15][i][0] for i in range(len(fd[15]))]
+Fuel2 = [fd[15][i][0] for i in range(len(fd[14]))]  #Engine 2
 
-Deflection_of_elevator = [fd[16][i][0] for i in range(len(fd[16]))]
+Deflection_of_aileron = [fd[16][i][0] for i in range(len(fd[15]))]
 
-Deflection_of_rudder = [fd[17][i][0] for i in range(len(fd[17]))]
+Deflection_of_elevator = [fd[17][i][0] for i in range(len(fd[16]))]
 
-#UTC Date DD:MM:YY = [fd[18][i][0] for i in range(len(fd[18]))]
+Deflection_of_rudder = [fd[18][i][0] for i in range(len(fd[17]))]
 
-UTC_Seconds = [fd[19][i][0] for i in range(len(fd[19]))]
+#UTC Date DD:MM:YY = [fd[19][i][0] for i in range(len(fd[18]))]
 
-Roll_Angle = [fd[20][i][0] for i in range(len(fd[20]))]
+UTC_Seconds = [fd[20][i][0] for i in range(len(fd[19]))]
 
-Pitch_Angle = [fd[21][i][0] for i in range(len(fd[21]))]
+Roll_Angle = [fd[21][i][0] for i in range(len(fd[20]))]
 
-#<no description> = [fd[22][i][0] for i in range(len(fd[22]))]
+Pitch_Angle = [fd[22][i][0] for i in range(len(fd[21]))]
 
-GNSS_Latitude = [fd[23][i][0] for i in range(len(fd[23]))]
+#<no description> = [fd[23][i][0] for i in range(len(fd[22]))]
 
-GNSS_Longitude = [fd[24][i][0] for i in range(len(fd[24]))]
+GNSS_Latitude = [fd[24][i][0] for i in range(len(fd[23]))]
 
-Body_Roll_Rate = [fd[25][i][0] for i in range(len(fd[25]))]
+GNSS_Longitude = [fd[25][i][0] for i in range(len(fd[24]))]
 
-Body_Pitch_Rate = [fd[26][i][0] for i in range(len(fd[26]))]
+Body_Roll_Rate = [fd[26][i][0] for i in range(len(fd[25]))]
 
-Body_Yaw_Rate = [fd[27][i][0] for i in range(len(fd[27]))]
+Body_Pitch_Rate = [fd[27][i][0] for i in range(len(fd[26]))]
 
-Body_Long_Accel = [fd[28][i][0] for i in range(len(fd[28]))]
+Body_Yaw_Rate = [fd[28][i][0] for i in range(len(fd[27]))]
 
-Body_Lat_Accel = [fd[29][i][0] for i in range(len(fd[29]))]
+Body_Long_Accel = [fd[29][i][0] for i in range(len(fd[28]))]
 
-Body_Norm_Accel = [fd[30][i][0] for i in range(len(fd[30]))]
+Body_Lat_Accel = [fd[30][i][0] for i in range(len(fd[29]))]
 
-Along_Heading_Accel = [fd[31][i][0] for i in range(len(fd[31]))]
+Body_Norm_Accel = [fd[31][i][0] for i in range(len(fd[30]))]
 
-Cros_Heading_Accel = [fd[32][i][0] for i in range(len(fd[32]))]
+Along_Heading_Accel = [fd[32][i][0] for i in range(len(fd[31]))]
 
-Vertical_Accel = [fd[33][i][0] for i in range(len(fd[33]))]
+Cros_Heading_Accel = [fd[33][i][0] for i in range(len(fd[32]))]
 
-Static_Air_Temperature = [fd[34][i][0] for i in range(len(fd[34]))]
+Vertical_Accel = [fd[34][i][0] for i in range(len(fd[33]))]
 
-Total_Air_Temperature = [fd[35][i][0] for i in range(len(fd[35]))]
+Static_Air_Temperature = [fd[35][i][0] for i in range(len(fd[34]))]
 
-Pressure_Altitude = [fd[36][i][0] for i in range(len(fd[36]))]          #(1013.25 mB)
+Total_Air_Temperature = [fd[36][i][0] for i in range(len(fd[35]))]
 
-Baro_Corrected_Altitude  = [fd[37][i][0] for i in range(len(fd[37]))] 
+Pressure_Altitude = [fd[37][i][0] for i in range(len(fd[36]))]          #(1013.25 mB)
 
-#<no description> = [fd[38][i][0] for i in range(len(fd[38]))]
+Baro_Corrected_Altitude  = [fd[38][i][0] for i in range(len(fd[37]))] 
 
-Mach = [fd[39][i][0] for i in range(len(fd[39]))]
+#<no description> = [fd[39][i][0] for i in range(len(fd[38]))]
 
-Computed_Airspeed = [fd[40][i][0] for i in range(len(fd[40]))]
+Mach = [fd[40][i][0] for i in range(len(fd[39]))]
 
-true_Airspeed = [fd[41][i][0] for i in range(len(fd[41]))]
+Computed_Airspeed = [fd[41][i][0] for i in range(len(fd[40]))]
 
-Altitude_Rate = [fd[42][i][0] for i in range(len(fd[42]))]
+true_Airspeed = [fd[42][i][0] for i in range(len(fd[41]))]
 
-Measurement_Running = [fd[43][i][0] for i in range(len(fd[43]))]
+Altitude_Rate = [fd[43][i][0] for i in range(len(fd[42]))]
+
+Measurement_Running = [fd[44][i][0] for i in range(len(fd[43]))]
 
 #Numberof Measurements Ready = []
 #Status of graph = []
 #Active Screen = []
 #T = []
-time = np.arange(0,43641,1)
+#time = np.arange(0,43641,1)
 
-plt.figure()
-plt.plot(time, Altitude_Rate)
-plt.show()
+#time = np.array(Pitch_Angle)/np.array(Body_Pitch_Rate
+print Deflection_of_Control_Column
+
+#plt.figure()
+#plt.plot(time, Altitude_Rate)
+# plt.show()
 
 
     
