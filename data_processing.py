87312e6d0cc8e736bf67d9421cf1ee4b66358e3e
Francesca <47746078+argenz@users.noreply.github.com>
2019-03-22 17:07:42 +0100

    Add files via upload
    update

--- data_processing.py
+++ data_processing.py
@@ -7,8 +7,12 @@ Created on Wed Mar 13 09:15:45 2019
 from readmat import *
 import matplotlib.pyplot as plt
 
-def normal_to_utc(hour, minute, second, millisecond):
+def clock_to_utc(hour, minute, second, millisecond):
     utc_time = (hour-1)*60**2 + minute*60 + second + millisecond*0.001
+    return utc_time
+
+def seconds_to_utc(s):
+    utc_time = 37856.910821 + s
     return utc_time
 
 def utc_to_normal(utc_time):
@@ -24,7 +28,7 @@ def utc_to_normal(utc_time):
 
 def find_index(utc_time):
     i = 0
-    if utc_time < UTC_Seconds[0]:
+    if utc_time <= UTC_Seconds[0]:
         raise ValueError('utc_time too low in find_index function')
     if utc_time > UTC_Seconds[-1]:
         raise ValueError('utc_time too high in find_index function')
@@ -47,16 +51,17 @@ def make_list(data_list, start_time, end_time):
     for i in range(start_index, end_index):
         y_values.append(data_list[i])
         time_values.append(UTC_Seconds[i]-start_time)
-    return[time_values, y_values]
+        
+    return(time_values, y_values)
 
 #phugoid
 #plt.figure(1)
 #phugoid = make_list(Pressure_Altitude, UTC_Seconds[0] + 2880, UTC_Seconds[0]+3020)
 #plt.plot(phugoid[0], phugoid[1])
-#
-plt.figure(2)
-elevator = make_list(Deflection_of_elevator, UTC_Seconds[0] + 2889, UTC_Seconds[0]+2908)
-plt.plot(elevator[0], elevator[1])
+##
+#plt.figure(2)
+#elevator = make_list(Deflection_of_elevator, UTC_Seconds[0] + 2889, UTC_Seconds[0]+2908)
+#plt.plot(elevator[0], elevator[1])
 #
 #plt.figure(3)
 #f1 = make_list(Fuel1, UTC_Seconds[0], UTC_Seconds[-1])
